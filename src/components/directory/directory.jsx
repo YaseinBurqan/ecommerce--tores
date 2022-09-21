989263cb062725b9014499ecacdc346d0597f8e8
Yasein Burqan <mr.yasein@gmail.com>
2022-09-22 00:13:04 +0300

    create category component and directory component

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,14 @@
+import "./directory.scss";
+import Categories from "../categories/categories";
+
+const Directory = () => {
+  return (
+    <>
+      <div className="directory">
+        <Categories />
+      </div>
+    </>
+  );
+};
+
+export default Directory;
